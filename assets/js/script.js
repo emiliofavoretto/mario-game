f922b9835722bee4c4a46504e257bda47c323f45
emilio <favorettoemilio@gmail.com>
2025-12-11 21:58:59 -0300

    finalização do contador de pontos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const pontosElement = document.querySelector('.pontos');
+
+let pontos = 0;
+let contouPonto = false;
 
 const jump = () => {
     mario.classList.add('jump');
@@ -24,10 +28,22 @@ const loop = setInterval(() => {
 
         mario.src = './assets/img/game-over.png';
         mario.style.width = '75px';
-        mario.style.marginLeft = '50px'; 
+        mario.style.marginLeft = '50px';
 
         clearInterval(loop);
     }
+
+    if (pipePosition < 50 && !contouPonto) {
+        pontos++; // soma 1 ponto
+        pontosElement.textContent = "pontos:" + pontos // atualiza o HTML
+        contouPonto = true; // impede contar várias vezes
+    }
+
+    // Quando o cano volta ao início, resetamos a contagem
+    if (pipePosition > 800) {
+        contouPonto = false;
+    }
+
 
 }, 10);
 
